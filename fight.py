51354d38a14a8d8bf8c1ab388da18c75c0642bdf
SarahTruffert <77342348+SarahTruffert@users.noreply.github.com>
2021-05-26 13:49:46 +0200

    Update fight.py

--- fight.py
+++ fight.py
@@ -1,28 +1,17 @@
-from .fight import fight
-class Battlefield():
-  """Battlefield used to battle two army against each other"""
 
-  def simpleFight(self, army, army2):
-    """
-    :param army: First army
-    :param army2: Second army
-    :return bool: Did the first army won ?
-    """
-    unit_1 = army[0]
-    unit_2 = army2[0]
-    
-    while True:
-        ret = fight(unit_1, unit_2)
-        if not ret:
-          unit_1 = army.pop()
-          if len(army)<1:
-            return False
-          unit_1 = army[0]
-          unit_2 = army2[0]
-        if ret :
-          unit_2 = army2.pop()
-          if len(army2)<1:
-            return True
-          unit_1 = army[0]
-          unit_2 = army2[0]
+def fight(unit_1, unit_2):
+  """
+  Simple Fight function
 
+  :param unit_1: First unit to fight
+  :param unit_2: Second unit to fight
+  :return bool: True if unit_1 won the fight False otherwise"""
+# counter = 10000
+
+  while unit_1.isalive():
+    unit_2.damaged(unit_1.strength)
+# counter -= 1
+    if not unit_2.isalive():
+      return True
+    unit_1.damaged(unit_2.strength)
+  return False
